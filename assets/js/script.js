ea761cb0c20749940938c753a556393b4b79a88d
Romain Cardot <romaincardot24000@gmail.com>
2022-11-07 15:38:47 +0100

    romain

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,15 @@
-let nav = document.querySelector("#navTuning")
-let stopbtn = document.getElementById("anim")
-let h1 = document.querySelector(".h1Anim")
-document.addEventListener('click', anim)
+let nav = document.querySelector("#navTuning");
+let stopbtn = document.getElementById("anim");
+let h1 = document.querySelector(".h1Anim");
+let imgNav = document.getElementById("imgNav");
+let imgNav2 = document.getElementById("imgNav2");
+document.addEventListener('click', anim);
 
 
 function anim(){
     nav.classList.toggle('headerBg');
-    h1.classList.toggle('h1Anim')
+    h1.classList.toggle('h1Anim');
+    imgNav.classList.toggle('displayNone');
+    imgNav2.classList.toggle('displayNone');
+    
 }
