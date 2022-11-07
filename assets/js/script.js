8ddacf79ea892208d3ca628eb6fa9d406f6eccf8
Fabrice Ho A Chuck <ragnastaroth@gmail.com>
2022-11-07 16:14:07 +0100

    ending commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,25 @@
 
 const minBtn = document.getElementById("carboChrono");
-const merguez = document.getElementById("merguez");
 
 minBtn.addEventListener("click", disco);
 
-minBtn.addEventListener('click', function(){
-        let merguez = document.getElementById("merguez");
-        player.play();
-});
+function disco() {
+    const merguez = document.getElementById("merguez");
+    merguez.play();
+}
+
 
 let nav = document.querySelector("#navTuning");
 let stopbtn = document.getElementById("anim");
-let h1 = document.querySelector(".h1Anim");
+let h1 = document.querySelector("h1");
 let imgNav = document.getElementById("imgNav");
 let imgNav2 = document.getElementById("imgNav2");
 document.addEventListener('click', anim);
-
 
 function anim(){
     nav.classList.toggle('headerBg');
     h1.classList.toggle('h1Anim');
     imgNav.classList.toggle('displayNone');
-    imgNav2.classList.toggle('displayNone');
-    
+    imgNav2.classList.toggle('displayNone');   
 }
 
