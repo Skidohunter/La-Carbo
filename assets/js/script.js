0b76f08090cfe055bda953abc16de3e3e51091dc
Fabrice Ho A Chuck <ragnastaroth@gmail.com>
2022-11-07 15:51:38 +0100

    commit html/css/js audio fab

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+const minBtn = document.getElementById("carboChrono");
+const merguez = document.getElementById("merguez");
+
+minBtn.addEventListener("click", disco);
+
+minBtn.addEventListener('click', function(){
+        let merguez = document.getElementById("merguez");
+        player.play();
+});
+
